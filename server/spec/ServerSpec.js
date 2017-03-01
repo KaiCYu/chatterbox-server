105f25fa92c5ec8c19049d0425b4dade846e3e4d
KaiCYu <kaichyu@gmail.com>
2017-02-28 19:57:25 -0800

    complete min specs, passing all tests

--- server/spec/ServerSpec.js
+++ server/spec/ServerSpec.js
@@ -96,6 +96,7 @@ describe('Node Server Request Listener Function', function() {
     expect(res._responseCode).to.equal(200);
     var messages = JSON.parse(res._data).results;
     expect(messages.length).to.be.above(0);
+
     expect(messages[0].username).to.equal('Jono');
     expect(messages[0].message).to.equal('Do my bidding!');
     expect(res._ended).to.equal(true);
@@ -113,7 +114,10 @@ describe('Node Server Request Listener Function', function() {
       function() { return res._ended; },
       function() {
         expect(res._responseCode).to.equal(404);
+        // done();
       });
+
+
   });
 
 });
